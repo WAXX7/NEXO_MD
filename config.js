c6691f89ce659421c4fd71c28c28ad9a9e5fff4b
𝐂ʀᴏɴᴀᴢ <waxx3832@gmail.com>
2025-01-11 15:51:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1725122591660_cw9Qqf_-0.png",
-ALIVE_MSG: process.env.ALIVE_MSG || " *CRONAZ-XD ALIVE🌸*\n\n*Owner:* White ser\n\n_Nothing Is Impossible. Motivate Your Own Self🙈_ ",
+ALIVE_MSG: process.env.ALIVE_MSG || " *NEXO-MD ALIVE🌸*\n\n*Owner:* nexo ser\n\n_Nothing Is Impossible. Motivate Your Own Self🙈_ ",
 OWNER_NAME: process.env.OWNER_NAME || "𝑾𝒉𝒊𝒕𝒆 𝒔𝒆𝒓",
-BOT_NAME: process.env.BOT_NAME || "CRONAZ-XD",
+BOT_NAME: process.env.BOT_NAME || "NEXO-XD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
